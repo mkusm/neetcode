17fb41933c2745de2e46a75a21818a2ea65b29b9
Michał Kuśmierczyk <kusm@protonmail.ch>
2024-07-26 21:30:14 +0200

    sliding window: simplify ex 03

--- 03_sliding_window/03_longest_repeating_substring_with_replacement.py
+++ 03_sliding_window/03_longest_repeating_substring_with_replacement.py
@@ -2,19 +2,6 @@ from collections import defaultdict
 
 
 def character_replacement(s: str, k: int) -> int:
-    def most_common_char_count(chars: dict[str, int]) -> str:
-        # max_ = 0
-        # max_char = ""
-        # for c in chars.keys():
-        #     if chars[c] > max_:
-        #         max_ = chars[c]
-        #         max_char = c
-        # return max_char
-        max_ = 0
-        for v in chars.values():
-            max_ = max(max_, v)
-        return max_
-
     l, r = 0, 0
     chars = defaultdict(int)
     longest = 0
@@ -24,7 +11,7 @@ def character_replacement(s: str, k: int) -> int:
         r += 1
 
         window_length = r - l
-        is_legit = window_length <= most_common_char_count(chars) + k
+        is_legit = window_length <= max(chars.values()) + k
 
         if is_legit:
             longest = max(longest, window_length)
